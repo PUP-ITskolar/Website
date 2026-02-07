88b4e320257fb039246f8706d5922c3978a24ee2
ITskolarNgBayan <itskolarngbayan@gmail.com>
2026-02-08 02:07:57 +0800

    Started working on the main menu window

--- time.js
+++ time.js
@@ -4,6 +4,16 @@ function time() {
     let m = time.getMinutes();
     let s = time.getSeconds();
 
+    if (h < 10) {
+        let hh = "0" + h;
+    }
+    if (m < 10) {
+        let mm = "0" + m;
+    }
+    if (s < 10) {
+        let ss = "0" + s;
+    }
+
 document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
 
 }
